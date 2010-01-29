5a2fdfc763164f3180bbcf4170c493eaf2090f8c
Nick Partridge <nkpart@gmail.com>
2010-01-29 13:45:56 +1000

    Testing inline mvn repo

--- project/build/inflector.scala
+++ project/build/inflector.scala
@@ -2,6 +2,7 @@ import sbt._
 
 class InflectorProject(info: ProjectInfo) extends DefaultProject(info) {
   
+  val publishTo = Resolver.file("in-repo-repo", ("repo").asFile)
   
   val repo = "nexus" at "http://nexus-direct.scala-tools.org/content/repositories/releases"
   
